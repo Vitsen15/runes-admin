89721c8e0aedfdd440403fad56e63c88ecddf667
Vitsentovich <vitsentovich.utd@gmail.com>
2017-11-12 12:08:23 +0200

    rewrite all to Russian in \ruses-order\update

--- views/runes-order/update.php
+++ views/runes-order/update.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\RunesOrder */
 
-$this->title = 'Update Runes Order: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Runes Orders', 'url' => ['index']];
+$this->title = 'Обновить порядок: ' . $model->runesWord->name;
+$this->params['breadcrumbs'][] = ['label' => 'Порядок рун', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Обновить';
 ?>
 <div class="runes-order-update">
 
