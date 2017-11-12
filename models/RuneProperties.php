838915a10222f8dd5620ab2cdabeb59631fb31de
Vitsentovich <vitsentovich.utd@gmail.com>
2017-11-12 11:30:40 +0200

    add rune_properties model

---
+++ models/RuneProperties.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the model class for table "rune_properties".
+ *
+ * @property integer $id
+ * @property string $property
+ * @property integer $in_weapon
+ * @property integer $in_armour
+ *
+ * @property RunesRuneProperties[] $runesRuneProperties
+ */
+class RuneProperties extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'rune_properties';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['property'], 'required'],
+            [['in_weapon', 'in_armour'], 'integer'],
+            [['property'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'property' => 'Property',
+            'in_weapon' => 'In Weapon',
+            'in_armour' => 'In Armour',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRunesRuneProperties()
+    {
+        return $this->hasMany(RunesRuneProperties::className(), ['property_id' => 'id']);
+    }
+}
