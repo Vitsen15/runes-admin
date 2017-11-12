ce044e0d54d0313a31c6b79a05727d917306ac6c
Vitsentovich <vitsentovich.utd@gmail.com>
2017-11-12 12:11:39 +0200

    rewrite to Russian \ruses-order\create

--- views/runes-order/create.php
+++ views/runes-order/create.php
@@ -6,8 +6,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\RunesOrder */
 
-$this->title = 'Create Runes Order';
-$this->params['breadcrumbs'][] = ['label' => 'Runes Orders', 'url' => ['index']];
+$this->title = 'Создать порядок рун';
+$this->params['breadcrumbs'][] = ['label' => 'Порядок рун', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="runes-order-create">
