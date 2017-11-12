99588ec45f5c4154c7160d8481b2556eaefcae7b
Vitsentovich <vitsentovich.utd@gmail.com>
2017-11-12 11:28:28 +0200

    add runes model

---
+++ models/Runes.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the model class for table "runes".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $img_url
+ * @property integer $lvl
+ *
+ * @property RunesOrder[] $runesOrders
+ * @property RunesRuneProperties[] $runesRuneProperties
+ */
+class Runes extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'runes';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'img_url', 'lvl'], 'required'],
+            [['lvl'], 'integer'],
+            [['name'], 'string', 'max' => 5],
+            [['img_url'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'img_url' => 'Img Url',
+            'lvl' => 'Lvl',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRunesOrders()
+    {
+        return $this->hasMany(RunesOrder::className(), ['rune_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRunesRuneProperties()
+    {
+        return $this->hasMany(RunesRuneProperties::className(), ['rune_id' => 'id']);
+    }
+}
