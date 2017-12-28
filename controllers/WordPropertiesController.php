5d97de2937e3a567edd171db6d6ed64b5bf56f13
Vitsentovich <vitsentovich.utd@gmail.com>
2017-12-28 23:28:51 +0200

    add redirect to index view in WordPropertiesController after creating property

--- controllers/WordPropertiesController.php
+++ controllers/WordPropertiesController.php
@@ -83,7 +83,7 @@ class WordPropertiesController extends Controller
         $model = new WordProperties();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['index']);
         } else {
             return $this->render('create', [
                 'model' => $model,
